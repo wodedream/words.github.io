3ea6378e3875904bccb2fddca4741178c6de4b5d
wodedream <45957303+wodedream@users.noreply.github.com>
2018-12-25 14:25:16 +0800

    Add files via upload

---
+++ html/test8/js/animatepic.js
@@ -0,0 +1,8 @@
+﻿//动态图片
+
+//if (!$first.is(":animated")) {
+//    if ((-marginLeft) > $first.width() + 1 + 18) {
+//        $first.css({ "margin-left": 0 }).appendTo($(".tgjlx ul"));
+//        marginLeft = 0;
+//    }
+//}
